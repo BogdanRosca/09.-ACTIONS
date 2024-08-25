8ec644c64911688e6317ab7b52d624d9ded24fa5
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 12:59:43 +0200

    use axio

--- .github/actions/get-cat/main.js
+++ .github/actions/get-cat/main.js
@@ -1,13 +1,11 @@
-//import axios from 'axios';
-
+const axios = require('axios')
 const core = require('@actions/core')
 const exec = require('@actions/exec')
 const github = require('@actions/github')
 
-function get_cat(){
-    // const response = await axios.get('http://example.com');
-    // return response
-    core.notice('cat')
+async function get_cat(){
+    const response = await axios.get('http://example.com'); 
+    core.notice(response)
 }
 
 get_cat()
