777415eb59640e4a4f316c9a527f586292900d77
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 14:35:57 +0200

    write to OUTPUT file

--- .github/actions/get-python-cat/main.py
+++ .github/actions/get-python-cat/main.py
@@ -5,7 +5,8 @@ def get_cat():
     url = os.environ['INPUT_URL']
     response = requests.get(url)
     fact = response.json().get('fact')
-    print(f'echo "fact={fact}" >> $GITHUB_OUTPUT')
+    with open(os.getenv('GITHUB_OUTPUT'), 'a') as output_file:
+        output_file.write(f"fact={fact}\n")
 
 if __name__ == '__main__':
     get_cat()
