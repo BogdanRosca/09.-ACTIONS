633df917d53ebd8fca9e20311ced2bc8d2a65853
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 13:30:39 +0200

    update package.json

--- .github/actions/get-cat/main.js
+++ .github/actions/get-cat/main.js
@@ -1,10 +1,9 @@
 const axios = require('axios')
 const core = require('@actions/core')
-const exec = require('@actions/exec')
 const github = require('@actions/github')
 
 async function get_cat(){
-    const url = core.getInput('url')
+    const url = core.getInput('url', { require: true })
     const response = await axios.get(url); 
     core.notice(response.data.fact)
 }
