e249db87a9270de54560f9b6336ced72ea3b5b70
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 13:06:10 +0200

    fix request

--- .github/actions/get-cat/main.js
+++ .github/actions/get-cat/main.js
@@ -4,8 +4,8 @@ const exec = require('@actions/exec')
 const github = require('@actions/github')
 
 async function get_cat(){
-    const response = await axios.get('http://example.com'); 
-    core.notice(response)
+    const response = await axios({ method: 'get', url: 'https://catfact.ninja/fact' }); 
+    core.notice(response.data.fact)
 }
 
 get_cat()
