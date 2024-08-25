7ea15dd8a7973ed92e42dc1210034c084b080da9
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 13:22:30 +0200

    fix input

--- .github/actions/get-cat/main.js
+++ .github/actions/get-cat/main.js
@@ -4,7 +4,7 @@ const exec = require('@actions/exec')
 const github = require('@actions/github')
 
 async function get_cat(){
-    const url = core.getInput(url)
+    const url = core.getInput('url')
     const response = await axios.get(url); 
     core.notice(response.data.fact)
 }
