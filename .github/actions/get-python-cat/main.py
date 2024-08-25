8215592c1775442d4d8c47e3d7f34dc80e2e1f7a
BogdanRosca <rosca.bogdan02@gmail.com>
2024-08-25 14:33:42 +0200

    move quotes

--- .github/actions/get-python-cat/main.py
+++ .github/actions/get-python-cat/main.py
@@ -5,7 +5,7 @@ def get_cat():
     url = os.environ['INPUT_URL']
     response = requests.get(url)
     fact = response.json().get('fact')
-    print(f'"echo fact={fact}" >> $GITHUB_OUTPUT')
+    print(f'echo "fact={fact}" >> $GITHUB_OUTPUT')
 
 if __name__ == '__main__':
     get_cat()
